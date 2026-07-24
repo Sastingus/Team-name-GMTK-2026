826a4b270c601193591b5c928c001f85657d015d
Sastingus <theflyingpigs10@gmail.com>
2026-07-23 19:44:17 -0500

    throwing

--- scenes/components/grab_component.gd
+++ scenes/components/grab_component.gd
@@ -6,6 +6,10 @@ class_name GrabComponent
 @export var disable_grabbing := false
 @export var station := Globals.Stations.KITCHEN
 
+var timer := 0.0
+var old_position := Vector2.ZERO 
+var tracked_velocity := Vector2.ZERO
+
 var hovering := false
 var grabbing := false
 
@@ -14,17 +18,27 @@ func _ready() -> void:
 	grab_area.mouse_entered.connect(hover)
 	grab_area.mouse_exited.connect(stop_hovering)
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	timer += delta
+	if timer >= 0.01:
+		tracked_velocity = (global_position-old_position)/timer
+		
+		timer = 0.0
+		old_position = global_position
+	
 	if disable_grabbing:
 		grabbing = false
+	
 	if station != Globals.Stations.KITCHEN:
 		if Globals.current_station != station:
 			grabbing = false
 	
 	if grabbing && Globals.mouse_down:
+		parent.freeze = true
 		parent.global_position = get_global_mouse_position()
 	if not Globals.mouse_down && grabbing:
-		parent.linear_velocity = Globals.mouse_velocity*Globals.THROW_STRENGTH
+		parent.freeze = false
+		parent.linear_velocity = tracked_velocity
 	if not Globals.mouse_down:
 		grabbing = false
 
