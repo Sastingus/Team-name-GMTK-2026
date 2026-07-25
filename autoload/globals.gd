2948610ddf88d576713f43b578582025c0dc3c3d
Sastingus <theflyingpigs10@gmail.com>
2026-07-25 18:37:38 -0500

    win and lose song

--- autoload/globals.gd
+++ autoload/globals.gd
@@ -6,13 +6,13 @@ var mouse_velocity := Vector2.ZERO
 const THROW_STRENGTH = 0.5
 
 var sent_food := []
-const RECIPE_COUNT = 7
+const RECIPE_COUNT = 1
 
 var current_recipe := 1
 
 var in_game := false
 var game_timer := 0.0
-const GAME_TIME = 150
+const GAME_TIME = 35
 
 var current_station : Stations = Stations.KITCHEN
 
--- scenes/worlds/world.gd
+++ scenes/worlds/world.gd
@@ -9,16 +9,24 @@ extends Node2D
 func _process(delta: float) -> void:
 	timer_label.text = str(round(Globals.game_timer*10)/10)
 	music_player()
+	if round(Globals.game_timer*10)/10 == 30.38:
+		fade_out()
+	if round(Globals.game_timer*10)/10 == 30:
+		fade_in()
 
 func _ready() -> void:
 	SignalBus.lose.connect(lose)
 	SignalBus.win.connect(win)
 
 func win():
+	audio_stream_player.stream = preload("res://assets/music/WinSong.mp3")
+	audio_stream_player.play()
 	win_label.visible = true
 	get_tree().paused = true
 
 func lose():
+	audio_stream_player.stream = preload("res://assets/music/LoseSong.mp3")
+	audio_stream_player.play()
 	lose_label.visible = true
 	explosion.play("default")
 	get_tree().paused = true
@@ -34,7 +42,16 @@ func music_player():
 		if not audio_stream_player.stream == load("res://assets/music/A-Song.mp3"):
 			audio_stream_player.stream = preload("res://assets/music/A-Song.mp3")
 			audio_stream_player.play()
-	else:
+	elif Globals.game_timer <= 30 and not Globals.game_timer <= 0 and not win_label.visible:
 		if not audio_stream_player.stream == load("res://assets/music/SONG3BS2WT.mp3"):
 			audio_stream_player.stream = preload("res://assets/music/SONG3BS2WT.mp3")
 			audio_stream_player.play()
+
+
+func fade_out():
+	var tween = get_tree().create_tween()
+	tween.tween_property(audio_stream_player,"volume_db",-30,0.25)
+
+func fade_in():
+	var tween = get_tree().create_tween()
+	tween.tween_property(audio_stream_player,"volume_db",0,0.25)
