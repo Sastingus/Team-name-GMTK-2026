14d38958e832a34a628badf6d0d42e0bac35bdea
TheFosterKid <lanceraikoglo3@gmail.com>
2026-07-24 18:51:20 -0700

    im commiting music fix

--- scenes/worlds/world.gd
+++ scenes/worlds/world.gd
@@ -4,9 +4,11 @@ extends Node2D
 @onready var explosion: AnimatedSprite2D = $Ui/LoseLabel/Explosion
 @onready var lose_label: Label = $Ui/LoseLabel
 @onready var win_label: Label = $Ui/WinLabel
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _process(delta: float) -> void:
 	timer_label.text = str(round(Globals.game_timer*10)/10)
+	music_player()
 
 func _ready() -> void:
 	SignalBus.lose.connect(lose)
@@ -23,3 +25,15 @@ func lose():
 func _on_restart_button_pressed() -> void:
 	Globals._ready()
 	get_tree().change_scene_to_packed(preload("res://scenes/worlds/main_menu.tscn"))
+	
+func music_player():
+	
+	if Globals.game_timer >= 30:
+		if not audio_stream_player.stream == load("res://assets/mUSIC/A-Song.mp3"):
+			audio_stream_player.stream = preload("res://assets/mUSIC/A-Song.mp3")
+			audio_stream_player.play()
+	else:
+		if not audio_stream_player.stream == load("res://assets/mUSIC/SONG3BS2WT.mp3"):
+			audio_stream_player.stream = preload("res://assets/mUSIC/SONG3BS2WT.mp3")
+			audio_stream_player.play()
+			
