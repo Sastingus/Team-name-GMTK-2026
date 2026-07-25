79b5fa2d44b09a51a1ec2a375b2d0e6cc33d8f2d
Sastingus <theflyingpigs10@gmail.com>
2026-07-25 13:50:55 -0500

    recipes
    1. stuffing
    2. turkey
    3. deviled egg

--- scenes/components/grab_component.gd
+++ scenes/components/grab_component.gd
@@ -36,16 +36,14 @@ func _process(delta: float) -> void:
 			grabbing = false
 	
 	if grabbing && Globals.mouse_down:
-		parent.freeze = true
 		parent.global_position = get_global_mouse_position()
 	if not Globals.mouse_down && grabbing:
 		if Globals.current_station != Globals.Stations.KITCHEN:
 			parent.global_position = get_global_mouse_position()
-			parent.freeze = false
 			parent.linear_velocity = tracked_velocity
 			grabbing = false
 		else:
-			parent.freeze = true
+
 			parent.global_position = get_global_mouse_position()
 
 
