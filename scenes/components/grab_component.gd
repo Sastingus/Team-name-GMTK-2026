fb2e70348ee50fbdca034a4998c1af3706e94e87
Sastingus <theflyingpigs10@gmail.com>
2026-07-25 20:54:00 -0500

    microwave multiple work

--- scenes/components/grab_component.gd
+++ scenes/components/grab_component.gd
@@ -38,7 +38,7 @@ func _process(delta: float) -> void:
 	if grabbing && Globals.mouse_down:
 		parent.global_position = get_global_mouse_position()
 	if not Globals.mouse_down && grabbing:
-		if Globals.current_station != Globals.Stations.KITCHEN:
+		if Globals.current_station != Globals.Stations.KITCHEN and Globals.current_station != Globals.Stations.RECIPE:
 			parent.global_position = get_global_mouse_position()
 			parent.linear_velocity = tracked_velocity
 			grabbing = false
--- scenes/tools/microwave/microwave.gd
+++ scenes/tools/microwave/microwave.gd
@@ -1,10 +1,10 @@
 extends Area2D
 class_name Microwave
 
-var current_target : RigidBody2D
+var current_target := []
 var current_temp := 0.0
 var microwave_started := false
-var microwave_opened := false
+var microwave_opened := true
 
 const HEAT_PER_SECOND = 50
 const DOOR_MOVE_DISTANCE = 200
@@ -26,7 +26,7 @@ func _process(delta: float) -> void:
 		door_button.disabled = false
 
 func get_microwaveable() -> bool:
-	if current_target != null:
+	if current_target.size() >= 1:
 		if microwave_opened:
 			if microwave_started:
 				return true
@@ -35,22 +35,28 @@ func get_microwaveable() -> bool:
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_node("Microwaveable_Component"):
 		if not microwave_opened:
-			current_target = body
-
+			if not body in current_target:
+				current_target.append(body)
+				body.set_collision_mask_value(5,true)
 
 func _on_body_exited(body: Node2D) -> void:
-	if body == current_target:
-		current_target = null
+	if body in current_target:
+		body.z_index = 0
+		body.collision_layer = 0
+		current_target.erase(body)
+		body.set_collision_mask_value(5,false)
 
 func _on_start_button_pressed() -> void:
 	if microwave_opened:
 		microwave_started = true
+
 func _on_stop_button_pressed() -> void:
 	if get_microwaveable():
-		if current_target.get_node("Microwaveable_Component").target_heat+BURNT_HEAT <= current_temp:
-			current_target.get_node("Microwaveable_Component").burn()
-		elif current_target.get_node("Microwaveable_Component").target_heat <= current_temp:
-			current_target.get_node("Microwaveable_Component").microwave()
+		for target in current_target:
+			if target.get_node("Microwaveable_Component").target_heat+BURNT_HEAT <= current_temp:
+				target.get_node("Microwaveable_Component").burn()
+			elif target.get_node("Microwaveable_Component").target_heat <= current_temp:
+				target.get_node("Microwaveable_Component").microwave()
 	microwave_started = false
 	current_temp = 0
 
@@ -60,16 +66,18 @@ func _on_door_button_toggled(toggled_on: bool) -> void:
 		if microwave_opened:
 			door_button.modulate = Color(1.0, 1.0, 1.0, 0.7)
 			door_button.position += Vector2(door_button.size.x,0)
-			if current_target == null:
-				enable_walls(true)
+			if current_target.size() == 0:
+				disable_walls(true)
 				door_button.z_index = -1
 			else:
-				enable_walls(false)
-				door_button.z_index = 1
+				disable_walls(false)
+				microwave_reorder(-2)
+				door_button.z_index = -1
 		else:
 			door_button.modulate = Color(0.0, 0.0, 0.0, 1)
 			door_button.position -= Vector2(door_button.size.x,0)
-			enable_walls(false)
+			disable_walls(false)
+			microwave_reorder(0)
 			door_button.z_index = -1
 			recalculate_target()
 			
@@ -79,6 +87,10 @@ func recalculate_target():
 		if body.has_node("Microwaveable_Component"):
 			body_entered.emit(body)
 
-func enable_walls(yes):
+func disable_walls(yes):
 	for wall in microwave_walls.get_children():
 		wall.disabled = yes
+
+func microwave_reorder(idx:int):
+	for target in current_target:
+		target.z_index = idx
