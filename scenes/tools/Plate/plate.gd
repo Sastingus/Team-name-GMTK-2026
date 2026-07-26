116060d6ce95b56099676d35a3efd33170c5d462
Sastingus <theflyingpigs10@gmail.com>
2026-07-25 21:28:37 -0500

    plate multiple

--- scenes/tools/Plate/plate.gd
+++ scenes/tools/Plate/plate.gd
@@ -1,33 +1,37 @@
 extends StaticBody2D
 class_name Plate
 
-var current_target : RigidBody2D
+var current_target := []
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var button: Button = $Button
 
 func send_off():
-	if current_target != null:
-		current_target.reparent(sprite)
+	if current_target.size() > 0:
+		for target in current_target:
+			target.call_deferred("reparent",sprite)
 		animation_player.play("send_off")
 		if Globals.game_timer <= animation_player.current_animation_length + 0.25:
-			Globals.send_food(current_target.name)
+			for target in current_target:
+				Globals.send_food(target.name)
 
 func delete_target():
-	current_target.queue_free()
+	for target in current_target:
+		Globals.send_food(target.name)
+		target.queue_free()
+	current_target.clear()
 	animation_player.play("send_back")
-	Globals.send_food(current_target.name)
 
 
 func _on_plating_area_body_entered(body: Node2D) -> void:
 	if body.has_node("Ingredient_Component"):
 		if body.get_node("Ingredient_Component").finished:
-			current_target = body
+			current_target.append(body)
 			if animation_player.current_animation == "":
 				button.visible = true
 
 
 func _on_plating_area_body_exited(body: Node2D) -> void:
-	if body == current_target:
-		current_target = null
+	if body in current_target:
+		current_target.erase(body)
 		button.visible = false
